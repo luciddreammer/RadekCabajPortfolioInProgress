fa514aae2f5df9b8406414f514725a26072911b0
Szymon <szym.burkowicz@gmail.com>
2022-11-01 13:09:04 +0100

    sliding menu add

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function goTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function invisibleText(id) {
+    let textId = document.getElementById(id);
+    let checkState = window.getComputedStyle(textId, null).display;
+    if(window.getComputedStyle(textId, null).display === 'block')
+    {
+        textId.style.display = "none";
+    }
+    else if(window.getComputedStyle(textId, null).display === 'none')
+    {
+        textId.style.display = "block";
+    }
+}
